06a256d4a2ab46ab4e324d801ad881b1073417d5
asmitbiswas <asmitbiswas1308@gmail.com>
2026-06-27 21:17:24 +0530

    Test

---
+++ app/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Dev-Vault
+      </h1>
+      <p>
+        Welcome to Dev-Vault, your ultimate destination for all things development! Whether you're a seasoned developer or just starting your coding journey, Dev-Vault is here to provide you with valuable resources, and insights to enhance your skills and knowledge. Explore our curated content, stay updated with the latest trends in the tech world, and connect with a vibrant community of developers. Let's unlock the potential of your coding abilities together!
+      </p>
+        </div>
+  );
+}
