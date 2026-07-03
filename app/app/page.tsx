635c1a5984b40582ef2fea1c6a656b9b736be680
asmitbiswas <asmitbiswas1308@gmail.com>
2026-07-03 20:27:00 +0530

    Updated content of home-page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -33,13 +33,16 @@ export default function Home() {
 
     <h2>What's inside?</h2>
 
-    <ul>
-        <li>React documentation and hooks</li>
-        <li>Next.js App Router</li>
-        <li>JavaScript concepts</li>
-        <li>Node.js & Express</li>
-        <li>MongoDB & PostgreSQL</li>
-    </ul>
+    <p>
+
+    
+        React documentation and hooks
+        Next.js App Router
+        JavaScript concepts
+        Node.js & Express
+        MongoDB & PostgreSQL
+    
+    </p>
 
     <h2>Why I made this</h2>
 
