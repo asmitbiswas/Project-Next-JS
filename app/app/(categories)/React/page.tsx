4f54d5ebdcf4bd62eb4aaaa280a839945867c19e
asmitbiswas <asmitbiswas1308@gmail.com>
2026-07-07 18:31:13 +0530

    Used Api_Context page

--- app/app/(categories)/React/page.tsx
+++ app/app/(categories)/React/page.tsx
@@ -60,7 +60,7 @@ export default function React() {
           <li><Link href="/React/List_Keys" style={{ color: "white" }}>Lists & Keys</Link></li>
           <li><Link href="/React/Forms" style={{ color: "white" }}>Forms</Link></li>
           <li><Link href="/React/Hookss" style={{color:"white"}}>Hooks</Link></li>
-          <li>Context API</li>
+          <li><Link href="/React/Api_Context" style={{ color: "white" }}>Context API</Link></li>
           <li>React Router</li>
           <li>Performance Optimization</li>
         </ul>
