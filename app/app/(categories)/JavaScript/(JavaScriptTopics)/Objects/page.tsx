00b666b2a9091c8a83ac10013952045578e00353
asmitbiswas <asmitbiswas1308@gmail.com>
2026-07-29 19:16:27 +0530

    Styling pre'

--- app/app/(categories)/JavaScript/(JavaScriptTopics)/Objects/page.tsx
+++ app/app/(categories)/JavaScript/(JavaScriptTopics)/Objects/page.tsx
@@ -144,7 +144,15 @@ person.greet();`}
 
       <h2>Looping Through an Object</h2>
 
-      <pre>
+      <pre
+         style={{
+        background:"#111",
+        padding:"14px",
+        borderRadius:"12px",
+        overflowX:"auto",
+        borderLeft:"6px solid rgb(13 200 13)"
+      }}
+      >
 {`const person = {
     name: "Asmit",
     age: 13,
