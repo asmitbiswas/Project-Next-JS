a804c4c098dd9cc16828da413c5a74b754cd034b
asmitbiswas <asmitbiswas1308@gmail.com>
2026-07-29 18:53:37 +0530

    Styling pre'

--- app/app/(categories)/JavaScript/(JavaScriptTopics)/Objects/page.tsx
+++ app/app/(categories)/JavaScript/(JavaScriptTopics)/Objects/page.tsx
@@ -75,7 +75,15 @@ console.log(person);`}
 
       <h2>Updating a Property</h2>
 
-      <pre>
+      <pre
+        style={{
+        background:"#111",
+        padding:"14px",
+        borderRadius:"12px",
+        overflowX:"auto",
+        borderLeft:"6px solid rgb(13 200 13)"
+      }}
+      >
 {`const person = {
     name: "Asmit",
     age: 13
