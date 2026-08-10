2bc857690c3447377459c6cdc90bdefe98082044
asmitbiswas <asmitbiswas1308@gmail.com>
2026-08-10 21:35:14 +0530

    wrote FS  page

--- app/app/(categories)/Node.js/page.tsx
+++ app/app/(categories)/Node.js/page.tsx
@@ -150,7 +150,7 @@ server.listen(3000);`}</code>
                 <li><Link href="/Node.js/basics" style={{color:"white"}}>Node.js basics</Link></li>
                 <li><Link href="/Node.js/Modules" style={{color:"white"}}>Modules</Link></li>
                 <li><Link href="/Node.js/NPM" style={{color:"white"}}>npm</Link></li>
-                <li>File system</li>
+                <li><Link href="/Node.js/FS" style={{color:"white"}}>File system</Link></li>
                 <li>HTTP</li>
                 <li>Asynchronous programming</li>
                 <li>Express.js</li>
