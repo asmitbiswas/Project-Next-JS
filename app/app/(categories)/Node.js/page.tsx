867d9f4118b376d6ac37535f789d89d901b4b0af
asmitbiswas <asmitbiswas1308@gmail.com>
2026-08-11 20:46:35 +0530

    wrote EXJ  page

--- app/app/(categories)/Node.js/page.tsx
+++ app/app/(categories)/Node.js/page.tsx
@@ -153,7 +153,7 @@ server.listen(3000);`}</code>
                 <li><Link href="/Node.js/FS" style={{color:"white"}}>File system</Link></li>
                 <li><Link href='/Node.js/HTTP' style={{color:"white"}}>HTTP</Link></li>
                 <li><Link href='/Node.js/ASYNC' style={{color:"white"}}>Asynchronous programming</Link></li>
-                <li>Express.js</li>
+                <li>><Link href='/Node.js/EXJ' style={{color:"white"}}>Express.js</Link></li>
                 <li>REST APIs</li>
                 <li>Databases</li>
                 <li>Authentication</li>
